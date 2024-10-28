df4a8ac59a2b5eb7cb52024cc8f4fdecc13f0564
IamCOD3X <sourabh.panchal28@gmail.com>
2024-10-28 22:14:49 +0530

    Build: setting up

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,7 +4,11 @@
 export PLATFORM_VERSION=11
 export ANDROID_MAJOR_VERSION=r
 export ARCH=arm64
+export LOCALVERSION=-ViP3R🐍-v1.0-NETHUNTER-SPECIAL-EDITION
+export KBUILD_BUILD_USER=IamCOD3X
+export KBUILD_BUILD_HOST=RYZEN
+export DTC_EXT=/usr/bin/dtc
 
-make ARCH=arm64 exynos7885-a20_defconfig
-make ARCH=arm64 -j64
+make O=out ARCH=arm64 exynos7885-a20_defconfig
+make O=out ARCH=arm64 -j$(nproc --all)
 
